16adb496df346981e3f95cd072093cadbd5d9dfb
kyle-shk <guqtls85@naver.com>
2022-06-15 19:34:22 +0900

    coaches coachform

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,17 @@
-<template>lalala</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <CoachForm />
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
