f1caa389f4799ab857440eadc422ad7097b7aafe
kyle-shk <guqtls85@naver.com>
2022-06-15 16:50:52 +0900

    add ui components

--- src/components/coaches/CoachItem.vue
+++ src/components/coaches/CoachItem.vue
@@ -4,11 +4,19 @@
     <h3>{{ fullName }}</h3>
     <h4>${{ rate }}/hour</h4>
     <div>
-      <span v-for="area in areas" :key="area">{{ area }}</span>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+      ></base-badge>
     </div>
+
     <div class="actions">
-      <router-link :to="coachContachLink">Contact</router-link>
-      <router-link :to="coachDetailsLink">View Details</router-link>
+      <base-button mode="outline" link :to="coachContachLink"
+        >Contact</base-button
+      >
+      <base-button link :to="coachDetailsLink">View Details</base-button>
     </div>
   </li>
 </template>
