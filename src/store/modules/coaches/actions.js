61ec378dd6692458b2bbee7e998f8a56a48e7e9e
kyle-shk <guqtls85@naver.com>
2022-06-17 21:32:51 +0900

    sending put request to store coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId, // index.js의 id
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
@@ -9,6 +9,23 @@ export default {
       areas: data.areas,
     };
 
-    context.commit('registerCoach', coachData);
+    const response = await fetch(
+      `https://test-ex-674aa-default-rtdb.firebaseio.com/coaches/${userId}.json`, // firebase에서는 .json붙어야함
+      {
+        method: 'PUT', // 1코치당 1개의 user가 되야하기때문에 put
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    // const responseData = await response.json() -> vuex에서 처리하는부분이라 딱히 뭘 안해도될듯
+
+    if (!response.ok) {
+      // error...
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
   },
 };
